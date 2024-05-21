6af91c4b15ad9bb5fe95e25659ece089b1bf18dd
EsFrost <sigmafi.tech@gmail.com>
2024-05-21 11:36:05 +0200

    Fixed an issue with single images on mobile view

--- photostockage/src/app/components/SinglePolaroid.tsx
+++ photostockage/src/app/components/SinglePolaroid.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export const SinglePolaroid = () => {
   return (
-    <div className='mt-[5rem] min-h-[100vh] mx-auto'>
+    <div className='mt-[5rem] min-h-[100vh] mx-auto sm:min-h-0'>
       <div className='
                 border-solid border-1 border-gray-900 p-[15px] pb-[60px] shadow-[5px_15px_15px_rgb(225,225,225)] h-full relative
                 after:content-[attr(polaroid-caption)] after:absolute after:bottom-0 after:left-0 after:w-[100%] after:text-center after:p-[10px] after:text-[30px]
