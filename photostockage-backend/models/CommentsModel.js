8e387e0d37fdfd08bff5906e707e8fd93e6a6d66
Sigmund Frost <sigmafi.tech@gmail.com>
2024-10-20 12:24:33 +0200

    Todo update 20-10-24

--- photostockage-backend/models/CommentsModel.js
+++ photostockage-backend/models/CommentsModel.js
@@ -1,20 +1,17 @@
-const { pool } = require('../utils/db')
+const { pool } = require("../utils/db");
 
 function getPhotoComments(p_id) {
-    return pool.query(
-        `SELECT * FROM comments WHERE id_photo = $1 AND status = true`,
-        [p_id]
-    )
+  return pool.query(
+    `SELECT * FROM comments WHERE id_photo = $1 AND status = true`,
+    [p_id]
+  );
 }
 
 function getAllPhotoComments(p_id) {
-    return pool.query(
-        `SELECT * FROM comments WHERE id_photo = $1`,
-        [p_id]
-    )
+  return pool.query(`SELECT * FROM comments WHERE id_photo = $1`, [p_id]);
 }
 
 module.exports = {
-    getPhotoComments,
-    getAllPhotoComments
-}
+  getPhotoComments,
+  getAllPhotoComments,
+};
